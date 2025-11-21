7f5959f9ab1cc8f26880a1cb4efaa722bcb25f8e
arif <arifbillahmubin@gmail.com>
2025-11-21 23:40:00 +0600

    banner img button set

---
+++ src/pages/Home/HowItWorks/HowItWorks.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HowItWorks = () => {
+    return (
+        <div>
+            <h2 className='font-semibold text-primary text-3xl'>How it works</h2>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default HowItWorks;
--- src/pages/Home/banner/Banner.jsx
+++ src/pages/Home/banner/Banner.jsx
@@ -7,15 +7,33 @@ import banner2 from "../../../assets/banner/banner2.png"
 import banner3 from "../../../assets/banner/banner3.png"
 const Banner = () => {
     return (
-        <Carousel autoPlay={true} infiniteLoop={true}>
-            <div>
-                <img src={banner1} />
+        <Carousel autoPlay={true} infiniteLoop={true} className='pt-8'>
+            <div className='relative'>
+                <img src={banner1} className=''/>
+                <div className='absolute top-[80%] left-[7%]'>
+                    <div className='flex gap-5'>
+                        <button className='btn btn-primary rounded-2xl text-black hidden md:block'>Track Your Parcel</button>
+                        <button className='btn hidden md:block'>Be A Rider</button>
+                    </div>
+                </div>
             </div>
-            <div>
+            <div className='relative'>
                 <img src={banner2} />
+                <div className='absolute top-[80%] left-[7%]'>
+                    <div className='flex gap-5'>
+                        <button className='btn btn-primary rounded-2xl text-black hidden md:block'>Track Your Parcel</button>
+                        <button className='btn hidden md:block'>Be A Rider</button>
+                    </div>
+                </div>
             </div>
-            <div>
+            <div className='relative'>
                 <img src={banner3} />
+                <div className='absolute top-[80%] left-[7%]'>
+                    <div className='flex gap-5'>
+                        <button className='btn btn-primary rounded-2xl text-black hidden md:block'>Track Your Parcel</button>
+                        <button className='btn hidden md:block'>Be A Rider</button>
+                    </div>
+                </div>
             </div>
         </Carousel>
     );
